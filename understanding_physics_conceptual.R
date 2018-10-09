c8879237d74ebe8dd023af63c6f3594812581419
mixiaose <tuzi1570449128@hotmail.com>
2018-10-09 21:09:22 +0800

    到概念演变多维分析图，还缺少概念结构图

--- understanding_physics_conceptual.R
+++ understanding_physics_conceptual.R
@@ -5,6 +5,7 @@ library(magrittr)
 library(tidyr)
 library(readxl)
 library(dplyr)
+library(wordcloud2)
 # 数据载入
 My_paper_data <-
   readFiles("D:\\Data_analisis\\concept_understanding\\total.txt")
@@ -33,3 +34,28 @@ Number_of_paper_every_year_before1945<-Number_of_paper_every_year %>%
   filter(PY<=1990) %>%
   colSums()
 # 作者关键词共现年度演变研究
+  #筛选数据函数
+My_bibli_datalist<-list()
+year_vector<-seq(1991,2018,1)
+for (year in year_vector) {
+  My_bibli_datalist[[which(year_vector==year)]]<-My_paper_data_biblio %>%
+    filter(PY==year)
+}
+#Yearly occurrences of top keyword
+topKeyword<-KeywordGrowth(My_paper_data_biblio,Tag = "DE",sep = ";",top = 10)
+topKeyword_melt<-gather(topKeyword,key = "Key_Words",value = "Occurences",-Year)
+
+ggplot(data = topKeyword_melt,
+       aes(x=Year,
+           y=Occurences,
+           group=Key_Words,
+           color=Key_Words))+geom_line(size=1)+
+  scale_y_continuous(limits = c(0,30))+
+  theme_bw()
+# 关键词共现-DE and AB
+DE_occurences<-biblioNetwork(My_paper_data_biblio,analysis = "co-occurrences",network = "keywords",sep = ";")
+
+
+
+AB_TM_Data<-termExtraction(My_paper_data_biblio,Field = "AB")
+AB_occurences<-biblioNetwork(AB_TM_Data,analysis = "co-occurrences",network = "abstracts",sep = ";")
